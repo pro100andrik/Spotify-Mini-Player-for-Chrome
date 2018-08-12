44be8d726e0c2c29a19e5df2e6a21e456a3050d5
pro100andrik <42306687+pro100andrik@users.noreply.github.com>
2018-08-12 03:12:19 +0300

    Add files via upload

---
+++ background.js
@@ -0,0 +1,43 @@
+//var nid = 'main';
+//var ctd = false;
+//var extID = 'ppibnbapebejhnmplokgfhijhfdchhhc'; // catcher links extension
+//var appID = 'gkcknpgdmiigoagkcoglklgaagnpojed'; // this app
+var frame = 'none';
+
+// frame options
+chrome.storage.sync.get(function(items) {
+    if (items.showFrame !== undefined) {
+        frame = items.showFrame;
+    }
+});
+
+var app = function() {
+    chrome.app.window.create(
+        'index.html',
+        {
+            id: 'mainWindow',
+            innerBounds: { width: 960, height: 600 },
+            frame: { type: frame },
+            resizable: true
+        },
+
+        function(e) {
+            
+        }
+    );
+}
+
+chrome.app.runtime.onLaunched.addListener(function() {
+    app();
+});
+
+chrome.app.runtime.onRestarted.addListener(function() {
+    app();
+});
+
+// send cached links to webview
+chrome.runtime.onMessageExternal.addListener(function(request, sender) {
+    if (sender.id == extID) {
+        chrome.runtime.sendMessage(appID, request);
+    }
+});
---
+++ main.js
@@ -0,0 +1,59 @@
+//var webview = document.getElementById('main');
+//var bullet = document.getElementById('drag');
+//var extID = 'ppibnbapebejhnmplokgfhijhfdchhhc'; // catcher links extension
+//var appID = 'gkcknpgdmiigoagkcoglklgaagnpojed'; // this app
+var insert_style = '.window-overlay::-webkit-scrollbar{height:33px;width:33px}.window-overlay::-webkit-scrollbar-thumb{min-height:50px;background:rgba(255,255,255,1);border-radius:17px;border:10px solid transparent;background-clip:padding-box}.window-overlay::-webkit-scrollbar-track-piece{background:rgba(0,0,0,.5);border:10px solid transparent;background-clip:padding-box}.window-overlay::-webkit-scrollbar-track-piece:vertical:start{border-radius:17px 17px 0 0}.window-overlay::-webkit-scrollbar-track-piece:vertical:end{border-radius:0 0 17px 17px}';
+
+// hide bullet if window is framed
+chrome.storage.sync.get(function(items) {
+    if (items.showFrame !== undefined) {
+        frame = items.showFrame;
+    }
+    if (frame == 'chrome') {
+        bullet.style.display = 'none';
+    } else {
+        insert_style += '.header-btn.header-boards,.promo-nav{margin-left:38px!important}';
+    }
+});
+
+// send new-window-links to bronser
+webview.addEventListener('newwindow', function(e) {
+    e.stopImmediatePropagation();
+    window.open(e.targetUrl);
+});
+
+// hotkeys
+window.addEventListener('keydown', function(e) {
+    // Ctrl+R or F5
+    if (e.ctrlKey && e.keyCode == 82 || e.keyCode == 115) {
+        webview.reload();
+    }
+
+    // F11
+    if (e.keyCode == 122) {
+        if (chrome.app.window.current().isFullscreen()) {
+            chrome.app.window.current().restore();
+        } else {
+            chrome.app.window.current().fullscreen();
+        }
+    }
+});
+
+// fix webview lose focus
+window.addEventListener('focus', function(e) {
+    webview.focus();
+});
+
+// allow download
+webview.addEventListener('permissionrequest', function(e) {
+    if (e.permission === 'download') {
+        e.request.allow();
+    }
+});
+
+// open cached links
+chrome.runtime.onMessage.addListener(function(request, sender) {
+    if (sender.id == appID) {
+        webview.src = request;
+    }
+});
